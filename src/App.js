50cc391efd9e9712cd3b7637367934024b087a90
MarioNadalCasa <220056@iessanalberto.com>
2023-10-24 19:01:30 +0200

    todo correcto 24-10-2023

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 function App() {
   const [mail, setMail] = useState("");
   const [errorMail, setErrorMail] = useState("");
+
   function saveMail(event){
     setMail(event.target.value)
   }
+
   function comprobarMail(){
-    mail.includes("@") ? setErrorMail(<p>Invalid email address entered!</p>) : setErrorMail(<p></p>)
+    setErrorMail(mail.includes("@") ? <p>Valid email address!</p> : <p>Invalid email address entered!</p>)
   }
     return (
     <div className="App">
         <h1>Your mail</h1>
         <input type="text" onChange={saveMail} value={mail}/>
-        <button onCLick={comprobarMail}>Submit</button>
+        <button onClick={comprobarMail}>Submit</button>
         {errorMail}
     </div>
   );
