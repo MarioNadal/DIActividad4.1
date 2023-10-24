8b9e9253a521746cbc9de0999eb45566d57fdf05
MarioNadal <220056@iessanalberto.com>
2023-10-24 14:04:01 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
-  return (
+  const [mail, setMail] = useState("");
+  const [errorMail, setErrorMail] = useState("");
+  function saveMail(event){
+    setMail(event.target.value)
+  }
+  function comprobarMail(){
+    mail.includes("@") ? setErrorMail(<p>Invalid email address entered!</p>) : setErrorMail(<p></p>)
+  }
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Your mail</h1>
+        <input type="text" onChange={saveMail} value={mail}/>
+        <button onCLick={comprobarMail}>Submit</button>
+        {errorMail}
     </div>
   );
 }
